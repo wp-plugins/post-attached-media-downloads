de1749ea1415df6da604781d7043d8fd9ebe4ff0
Clorith <Clorith@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-09 09:46:16 +0000

    Fix an intentional typo in the language file used for testing
    git-svn-id: https://plugins.svn.wordpress.org/post-attached-media-downloads/trunk@962972 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- resources/js/langs/en.js
+++ resources/js/langs/en.js
@@ -1,8 +1,8 @@
 tinyMCE.addI18n(
 	'en.pamd', {
-		plugin      : 'Post Attached Media Downloads',
+		plugin          : 'Post Attached Media Downloads',
 		linktarget      : 'Download behavior',
-		linktargetself  : 'Open in currentze window',
+		linktargetself  : 'Open in current window',
 		linktargetblank : 'Open a new tab/window'
 	}
 );
