b6a7632219dd6b71fb5dc23725b5fbd76aeb4ddd
Clorith <Clorith@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-09 09:59:09 +0000

    1.2 update
    git-svn-id: https://plugins.svn.wordpress.org/post-attached-media-downloads/tags/1.2@962974 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ resources/js/langs/en.js
@@ -0,0 +1,8 @@
+tinyMCE.addI18n(
+	'en.pamd', {
+		plugin          : 'Post Attached Media Downloads',
+		linktarget      : 'Download behavior',
+		linktargetself  : 'Open in current window',
+		linktargetblank : 'Open a new tab/window'
+	}
+);
